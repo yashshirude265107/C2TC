fd731de6a8d8a083d3bffd251d2053a49637c622
yashshirude265107 <shirudeyash@2221>
2024-04-17 04:20:11 +0530

    First Commit

--- JN07/src/TNSIF/Main.java
+++ JN07/src/TNSIF/Main.java
@@ -3,7 +3,7 @@ package TNSIF;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello yash");
+		System.out.println("hello yash shirude");
 		// TODO Auto-generated method stub
 
 	}
---
+++ JN07/src/TNSIF/pro2.java
@@ -0,0 +1,16 @@
+package TNSIF;
+
+public class pro2 {
+    public double x, y;
+
+    public pro2() {
+        x =0.0f;
+        y =0.0;
+    }
+
+    public static void main(String args[]) {
+        pro2 te = new pro2(); 
+        System.out.println("numbers = " +"("+ te.y +" , "+ te.x+")"); 
+    }
+}
+
